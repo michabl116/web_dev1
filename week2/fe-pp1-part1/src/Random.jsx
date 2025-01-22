359d16e85c26fece0cb9a6c1ef9b8b4dae810cac
micha <michabl@metropolia.fi>
2025-01-23 00:23:04 +0200

    update

---
+++ week2/fe-pp1-part1/src/Random.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Random() {
+  return (
+    <div>
+        <Random min={1} max={6}/>
+        <Random min={1} max={100}/>
+    </div>
+  )
+}
+
+export default Random
