bad52d696182daa332db66bc3d85ded1499b9089
micha <michabl@metropolia.fi>
2025-01-24 10:01:05 +0200

    update

--- week2/api-cars/carHandlers.js
+++ week2/api-cars/carHandlers.js
@@ -1,57 +1,67 @@
-const Car = require("./carLib");
+const Dog = require("./carLib");
 
-const getAllCars = (req, res) => {
-    const cars = Car.getAll();
-    res.json(cars);
-  };
-  const createCar = (req, res) => {
-    const { model, color, age } = req.body;
-  
-    const newCar = Car.addOne(model, color, age);
-  
-    if (newCar) {
-      res.json(newCar);
-    } else {
-      res.status(500).json({ message: "Failed to create car" });
-    }
-  };
-  const getCarById = (req, res) => {
-    const carId = req.params.carId;
-    const car = Car.findById(carId);
-    if (car) {
-      res.json(car);
-    } else {
-      res.status(404).json({ message: 'Car not found' });
-    }
-  };
-  const updateCar = (req, res) => {
-    const carId = req.params.carId;
-  
-    const { model, color, age } = req.body;
-  
-    const updatedCar = Car.updateOneById(carId, { model, color, age });
-  
-    if (updatedCar) {
-      res.json(updatedCar);
-    } else {
-      res.status(404).json({ message: "Car not found" });
-    }
-  };
-  const deleteCar = (req, res) => {
-    const carId = req.params.carId;
-  
-    const isDeleted = Car.deleteOneById(carId);
-  
-    if (isDeleted) {
-      res.json({ message: "Car deleted successfully" });
-    } else {
-      res.status(404).json({ message: "Car not found" });
-    }
-  };
-  module.exports = {
-    getAllCars,
-    getCarById,
-    createCar,
-    updateCar,
-    deleteCar,
-  };
+// GET /dogs
+const getAllDogs = (req, res) => {
+  const dogs = Dog.getAll();
+  res.json(dogs);
+};
+
+// POST /dogs
+const createDog = (req, res) => {
+  const { name, weight } = req.body;
+
+  const newDog = Dog.addOne(name, weight);
+
+  if (newDog) {
+    res.json(newDog);
+  } else {
+    res.status(500).json({ message: "Failed to create dog" });
+  }
+};
+
+// GET /dogs/:dogId
+const getDogById = (req, res) => {
+  const dogId = req.params.dogId;
+  const dog = Dog.findById(dogId);
+  if (dog) {
+    res.json(dog);
+  } else {
+    res.status(404).json({ message: 'Dog not found' });
+  }
+};
+
+// PUT /dogs/:dogId
+const updateDog = (req, res) => {
+  const dogId = req.params.dogId;
+
+  const { name, weight } = req.body;
+
+  const updatedDog = Dog.updateOneById(dogId, { name, weight });
+
+  if (updatedDog) {
+    res.json(updatedDog);
+  } else {
+    res.status(404).json({ message: "Dog not found" });
+  }
+};
+
+// DELETE /dogs/:dogId
+const deleteDog = (req, res) => {
+  const dogId = req.params.dogId;
+
+  const isDeleted = Dog.deleteOneById(dogId);
+
+  if (isDeleted) {
+    res.json({ message: "Dog deleted successfully" });
+  } else {
+    res.status(404).json({ message: "Dog not found" });
+  }
+};
+
+module.exports = {
+  getAllDogs,
+  getDogById,
+  createDog,
+  updateDog,
+  deleteDog,
+};
